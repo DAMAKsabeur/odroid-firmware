11483782d6897d8867fd53d0d3855bdf20fffc36
Sabeur DAMAK <damak.sabeur@gmail.com>
2024-04-19 15:55:06 +0200

    fix bash

--- odroid-c4/blx_fix.sh
+++ odroid-c4/blx_fix.sh
@@ -1,23 +1,12 @@
-#!/bin/bash
-
-#bl2 file size 41K, bl21 file size 3K (file size not equal runtime size)
-#total 44K
-#after encrypt process, bl2 add 4K header, cut off 4K tail
-
-#bl30 limit 41K
-#bl301 limit 12K
-#bl2 limit 41K
-#bl21 limit 3K, but encrypt tool need 48K bl2.bin, so fix to 7168byte.
-
-#$7:name flag
+#!/bin/sh
 if [ "$7" = "bl30" ]; then
-	declare blx_bin_limit=40960
-	declare blx01_bin_limit=13312
+	blx_bin_limit=40960
+	blx01_bin_limit=13312
 	echo "blx_bin_limit" $blx_bin_limit
     echo "blx01_bin_limit" $blx01_bin_limit
 elif [ "$7" = "bl2" ]; then
-	declare blx_bin_limit=57344
-	declare blx01_bin_limit=4096
+	blx_bin_limit=57344
+	blx01_bin_limit=4096
 	echo "blx_bin_limit" $blx_bin_limit
     echo "blx01_bin_limit" $blx01_bin_limit
 else
@@ -26,10 +15,12 @@ else
 fi
 
 # blx_size: blx.bin size, zero_size: fill with zeros
-declare -i blx_size=`du -b $1 | awk '{print int($1)}'`
+blx_size=`du -b $1 | awk '{print int($1)}'`
 echo "blx_size" $blx_size
-declare -i zero_size=$blx_bin_limit-$blx_size
+
+zero_size=$(($blx_bin_limit-$blx_size))
 echo "zero_size" $zero_size
+
 dd if=/dev/zero of=$2 bs=1 count=$zero_size
 cat $1 $2 > $3
 echo "3" $3
@@ -37,8 +28,8 @@ echo "1" $1
 echo "2" $2
 rm $2
 
-declare -i blx01_size=`du -b $4 | awk '{print int($1)}'`
-declare -i zero_size_01=$blx01_bin_limit-$blx01_size
+blx01_size=`du -b $4 | awk '{print int($1)}'`
+zero_size_01=$(($blx01_bin_limit-$blx01_size))
 dd if=/dev/zero of=$2 bs=1 count=$zero_size_01
 cat $4 $2 > $5
 
